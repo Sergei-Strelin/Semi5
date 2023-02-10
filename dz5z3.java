47a1f0a495aab071988560da78ce536ea41a8655
Sergei <legal.666@mail.ru>
2023-02-10 17:20:59 +0300

    dz5z3

---
+++ dz5z3.java
@@ -0,0 +1,89 @@
+/*
+Задание 3
+* Реализовать алгоритм пирамидальной сортировки (HeapSort)
+*/
+
+import java.util.*;
+
+public class dz5z3 {
+    private static Random random = new Random();
+    public static void main(String args[])
+    {
+        
+        int arr[] = getRandomArray(10, 0, 100); 
+        System.out.println("Исходный массив: ");
+        printArray(arr);
+
+        dz5z3 ob = new dz5z3();
+        ob.sort(arr);
+
+        System.out.println("Упорядоченный массив: ");
+        printArray(arr);
+    }
+
+    public void sort(int arr[]) {
+        int n = arr.length;
+ 
+         
+        for (int i = n / 2 - 1; i >= 0; i--)
+             heapify(arr, n, i);
+ 
+          
+        for (int i=n-1; i>=0; i--)
+        {
+            
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+ 
+            
+            heapify(arr, i, 0);
+        }
+    }
+ 
+    
+    
+    void heapify(int arr[], int n, int i) {
+        int largest = i; 
+        int l = 2*i + 1; 
+        int r = 2*i + 2; 
+ 
+        
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+ 
+        
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+        
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+ 
+            
+            heapify(arr, n, largest);
+        }
+    }
+ 
+    
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+
+
+    
+    static int[] getRandomArray(int size, int min, int max){
+        int[] arr = new int[size];//
+        int index = 0;
+        while (index < size) {
+            arr[index] = random.nextInt(min, max);
+            index = index + 1;
+        }
+        return arr;
+    }    
+ 
+}
